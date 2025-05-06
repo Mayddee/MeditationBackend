386612c728f144c1ac907c3b336b55de689dd92f
Mayddee <a.madikosh2004@gmail.com>
2025-05-06 21:13:30 +0500

    commit

--- src/database.py
+++ src/database.py
@@ -1,15 +1,18 @@
 import asyncio
+import os
 
 from fastapi import Depends, APIRouter
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
 from sqlalchemy.sql.annotation import Annotated
+from dotenv import load_dotenv
 
 from src.configDB import settings
 
 router = APIRouter()
+URL_DATABASE = os.getenv("URL_DATABASE")
 
-engine = create_async_engine(settings.DATABASE_URL_asyncpg, echo=False)
+engine = create_async_engine(URL_DATABASE)
 
 session_factory = async_sessionmaker(engine, class_=AsyncSession)
 
